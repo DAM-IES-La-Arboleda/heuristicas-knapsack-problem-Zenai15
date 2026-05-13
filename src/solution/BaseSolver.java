1b959de0c8d29fb966cf0fae0d683a52e5bfc434
Guille <guillegrear@gmail.com>
2026-05-13 17:57:24 +0200

    Arreglado bug

--- src/solution/BaseSolver.java
+++ src/solution/BaseSolver.java
@@ -2,7 +2,6 @@ package solution;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import java.util.TreeMap;
 
 import core.Item;
@@ -21,7 +20,8 @@ public class BaseSolver implements KnapsackSolver {
 		}
 
 		while (currentWeight <= capacity) {
-			selectedItems.add(lista.get(lista.pollFirstEntry()));
+			selectedItems.add(lista.get(lista.firstKey()));
+			lista.remove(lista.firstKey());
 		}
 
 		return selectedItems;
